9248094ddc3cf29a5434bedfd71307ed241518b4
Arslan Ahmed <arslangrt2018@gmail.com>
2026-05-23 20:01:27 +0400

    admin.py

---
+++ admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from .models import (
+    Course,
+    Module,
+    Lesson,
+    Question,
+    Choice,
+    Quiz,
+    Category,
+)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("id", "text")
+    search_fields = ("text",)
+    inlines = [ChoiceInline]
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+    search_fields = ("title",)
+    inlines = [QuestionInline]
