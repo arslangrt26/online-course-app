4e1b318ace474cb1c4fe792955b7b4ff4a95cbb8
Arslan Ahmed <arslangrt2018@gmail.com>
2026-05-23 20:47:38 +0400

    models.py

--- models.py
+++ models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
 
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
 class Question(models.Model):
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="questions"
+    )
     text = models.CharField(max_length=255)
+    grade = models.IntegerField()
 
     def __str__(self):
         return self.text
@@ -14,11 +27,18 @@ class Choice(models.Model):
         on_delete=models.CASCADE,
         related_name="choices"
     )
-    text = models.CharField(max_length=255)
+    choice_text = models.CharField(max_length=100)
     is_correct = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.text} ({'Correct' if self.is_correct else 'Wrong'})"
+        return f"{self.choice_text} ({'Correct' if self.is_correct else 'Wrong'})"
+
+
+class Enrollment(models.Model):
+    student_name = models.CharField(max_length=255)  # adjust if you have a User model
+
+    def __str__(self):
+        return self.student_name
 
 
 class Submission(models.Model):
@@ -27,10 +47,12 @@ class Submission(models.Model):
         on_delete=models.CASCADE,
         related_name="submissions"
     )
-    selected_choice = models.ForeignKey(
-        Choice,
-        on_delete=models.CASCADE
+    enrollment = models.ForeignKey(
+        Enrollment,
+        on_delete=models.CASCADE,
+        related_name="submissions"
     )
+    selected_choices = models.ManyToManyField(Choice)
     submitted_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
